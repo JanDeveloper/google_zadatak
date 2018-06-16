f898a9189c74694c660f9f4c8ca86db1df007d4b
JanDeveloper <sojak@mts.rs>
2018-06-17 00:49:15 +0200

    Updated

--- index.js
+++ index.js
@@ -1,57 +1,76 @@
+var a;
+var lt;
+var lg;
+var venue;
+var urlMap;
+var url;
 var map;
+var pos;
+var val;
+var rad;
 var infowindow;
-var message;
 $(document).ready(function () {
-    $(".button-find").click(function () {
-        initMap($("#myText").val(), $("#radius").val());
-    });
-});
-
-function initMap(value, radius) {
-    var pyrmont = {lat: 45.109835, lng: 20.727431};
-
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: pyrmont,
-        zoom: 15,
-    });
-    var marker = new google.maps.Marker({
-        position: pyrmont,
-        map: map,
-        title: "Your position."
-    });
-
-    infowindow = new google.maps.InfoWindow();
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch({
-        location: pyrmont,
-        radius: radius ? radius : 1000,
-        type: value
-    }, callback);
-
-}
-
-function callback(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
+  $(".button-find").click(function () {
+    if($('#myText').val() === '') {
+      $(".warnval").text("Please enter category.").show();
+      $('#myText').focus();
+      return false;
+  }else{
+    $(".warnval").hide();
+  }
+    $(".button-find").hide();
+    $(".button").append('<button class="reload">Refresh</button>');
+    urlMap = "https://api.foursquare.com/v2/venues/search?v=20180614&ll=45.109835,20.727431&intent=browse&client_id=U3TBPCTLGLQQ3MEFJLX3QZH3QESZEVHHOEXYWIIBG1YLPCHT&client_secret=DBNRFLKSMTTBVJ5G0VUYFUBTWKG5XOU5TPPL10PJYAI3LIUU";
+    var val = '&query=' + $('#myText').val();
+    var rad = '&radius=' + $('#radius').val();
+    url = urlMap + rad + val;
+     $.ajax({
+        url: url,
+        dataType: 'json',
+        success: function(data){
+          var venues = data.response.venues;
+          $.each(venues, function(i,venue){
+            $('#result').append('<p>' + venue.name + '<br />Address: ' + venue.location.address + '<br />City: ' + venue.location.city + '<br />Distance: ' + venue.location.distance + ' meters.</p><br />');
+            lt = venue.location.lat;
+            lg = venue.location.lng;
+            a = {lat:(lt), lng:(lg)};
+            pos = venue.name;
+  
+           var markers = new google.maps.Marker({
+            position: a,
+            map: map,
+            title: pos,
+        });
+       
+          });
+          $(".reload").click(function () {
+            location.reload();
+            $(".reload").delete();
+            $(".button-find").show();
+            $.ajax({
+              url: url,
+              dataType: 'json',
+              success: function(){}
+            });
+          })
 
         }
-    }
-}
+      })
+  })
 
-function createMarker(place) {
-    var placeLoc = place.geometry.location;
-    var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location,
-        title: place.name,
-    });
-
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-            'Adresa: ' + place.vicinity + '<br>'+
-            place.scope +'</div>');
-        infowindow.open(map, this);
-    });
-
+});
+function initMap(value, radius) {
+  
+  var myPosition = {lat: 45.109835, lng: 20.727431}
+  map = new google.maps.Map(document.getElementById('map'), {
+      center: myPosition,
+      zoom: 14,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+  var marker = new google.maps.Marker({
+      position: myPosition,
+      map: map,
+      title: "Your position."
+  });
+  
 }
